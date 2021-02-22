a0d4e9e1d9087ea71cf019e0082cfdb3228d11c6
somchay <55747347+somchaynoy@users.noreply.github.com>
2021-02-22 12:01:23 +0700

    Initial

--- index.php
+++ index.php
@@ -1,3 +1,39 @@
 <?php
-echo 'ok';
-echo 'sssss';
+require_once('./vendor/autoload.php');
+//ข้อความ
+//Namespace
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+
+$channel_token=
+'dxeiSAPWx/H9gQSlxjnCf2V1h3HJTsQSAEa2ldaJGaChzobAsbnVHa7XSbI8kCVZ84W+AUcSh/zwF3rYjVw2TDIBuW/yRRSkYgi6KuzYqDcm5mq5XJ/KsWDOCjS2eJkuow7c7EBM7AZYgfS3ohbJNAdB04t89/1O/w1cDnyilFU=';
+$channel_secret='2d65520b351de78be9667da60616b9fa';
+
+//Get message frome Line API
+$content=file_get_contents('php://input');
+$events=json_decode($content, true);
+
+if(!is_null($events['events'])){
+	//LOOP
+	foreach($events['events']as $event){
+		//Line API
+		if($event['type']=='message'){
+			switch($event['message']['type']){
+				case 'text' :
+				//Get re
+				$replyToken=$event['replyToken'];
+				
+				//Reply me
+				$respMessage='Hello, your message is '.$event['message']['text'];
+				
+				$httpClient=newCurlHTTPClient($channel_token);
+				$bot=newLINEBot($httpClient, array('channelSecret'=>$channel_secret));
+				$textMessageBuilder=newTextMessageBuilder($respMessage);
+				$response=$bot->replyMessage($replyToken,$textMessageBuilder);
+				break;
+				}
+			}
+		}
+	}
+echo "OK";
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '77f26e8ec06959bfc05760078eec3b21025a657b',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '77f26e8ec06959bfc05760078eec3b21025a657b',
+    ),
+    'linecorp/line-bot-sdk' => 
+    array (
+      'pretty_version' => '6.2.0',
+      'version' => '6.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b9d9a0d889c4a83b4521327882bf60be38b476e3',
+    ),
+  ),
+);
