e41f3cd65a840856dc96820d21ec22eb1e55588e
somchay <55747347+somchaynoy@users.noreply.github.com>
2021-02-22 16:54:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -27,9 +27,9 @@ if(!is_null($events['events'])){
 				//Reply me
 				$respMessage='Hello, your message is '.$event['message']['text'];
 				
-				$httpClient=newCurlHTTPClient($channel_token);
-				$bot=newLINEBot($httpClient, array('channelSecret'=>$channel_secret));
-				$textMessageBuilder=newTextMessageBuilder($respMessage);
+				$httpClient=new CurlHTTPClient($channel_token);
+				$bot=new LINEBot($httpClient, array('channelSecret'=>$channel_secret));
+				$textMessageBuilder=new TextMessageBuilder($respMessage);
 				$response=$bot->replyMessage($replyToken,$textMessageBuilder);
 				break;
 				}
