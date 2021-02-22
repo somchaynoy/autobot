15d7973aed06d1493262af579c1912a00ead2f28
somchay <55747347+somchaynoy@users.noreply.github.com>
2021-02-22 17:27:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,39 +1,46 @@
 <?php
 require_once('./vendor/autoload.php');
-//ข้อความ
+//รูป
 //Namespace
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 
 $channel_token=
-'8OePlDix8JOWu/AFFezoV7G5n2TgAhwy6UC4YYwB+ZgEn4tI5zBsUllBo3RyicuTx5mxknthQOaNgMiDj1Oxvhu9a5tGi5f4dBuZVsKiGAdUNrL53Wc9s5wKrl/Z2S1vhR/XciGUZ0CQysHvkL4Q6wdB04t89/1O/w1cDnyilFU=';
-$channel_secret='7892716414d782ce55cb0076ed4d536b';
+'dxeiSAPWx/H9gQSlxjnCf2V1h3HJTsQSAEa2ldaJGaChzobAsbnVHa7XSbI8kCVZ84W+AUcSh/zwF3rYjVw2TDIBuW/yRRSkYgi6KuzYqDcm5mq5XJ/KsWDOCjS2eJkuow7c7EBM7AZYgfS3ohbJNAdB04t89/1O/w1cDnyilFU=';
+$channel_secret='2d65520b351de78be9667da60616b9fa';
 
 //Get message frome Line API
 $content=file_get_contents('php://input');
 $events=json_decode($content, true);
 
 if(!is_null($events['events'])){
+	
 	//LOOP
 	foreach($events['events']as $event){
+		
 		//Line API
 		if($event['type']=='message'){
+			
+			$replyToken=$event['replyToken'];
+			
 			switch($event['message']['type']){
-				case 'text' :
-				//Get re
-				$replyToken=$event['replyToken'];
-				
-				//Reply me
-				$respMessage='Hello, your message is '.$event['message']['text'];
-				
-				$httpClient=new CurlHTTPClient($channel_token);
-				$bot=new LINEBot($httpClient, array('channelSecret'=>$channel_secret));
-				$textMessageBuilder=new TextMessageBuilder($respMessage);
-				$response=$bot->replyMessage($replyToken,$textMessageBuilder);
+					
+				case 'image':
+					$messageID=$event['message']['id'];
+					$respMessage='Hello, your image ID is '.$messageID;
+				break;
+				default:
+					$respMessage='Please send image only';
+			
 				break;
 				}
+			$httpClient=new CurlHTTPClient($channel_token);
+			$bot=new LINEBot($httpClient, array('channelSecret'=> $channel_secret));
+			$textMessageBuilder=new TextMessageBuilder($respMessage);
+			$response=$bot->replyMessage($replyToken, $textMessageBuilder);
 			}
 		}
 	}
 echo "OK";
+
