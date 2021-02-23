a51788893dd6a9cbfb87fb9c0ded9560f37b1cb1
somchay <55747347+somchaynoy@users.noreply.github.com>
2021-02-23 11:09:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,39 +1,13 @@
-<?php
-
-require_once('./vendor/autoload.php');
-
-//Namespace
-use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use \LINE\LINEBot;
-use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
-
-//Token
-$channel_token='8OePlDix8JOWu/AFFezoV7G5n2TgAhwy6UC4YYwB+ZgEn4tI5zBsUllBo3RyicuTx5mxknthQOaNgMiDj1Oxvhu9a5tGi5f4dBuZVsKiGAdUNrL53Wc9s5wKrl/Z2S1vhR/XciGUZ0CQysHvkL4Q6wdB04t89/1O/w1cDnyilFU=';
-
-$channel_secret='7892716414d782ce55cb0076ed4d536b';
-
-//Get
-$content=file_get_contents('php://input');
-$events=json_decode($content, true);
-
-if(!is_null($events['events'])){
-	
-	//Loop
-	foreach($events['events']as $event){
-		
-		//Get
-		$replyToken=$event['replyToken'];
-		
-		//Sticker
-		$packageID=1;
-		$stickerID=410;
-		
-		$httpClient=new CurlHTTPClient($channel_token);
-		$bot=new LINEBot($httpClient, array('channelSecret'=>$channel_secret));
-		
-		$textMessageBuilder=new StickerMessageBuilder($packageID, $stickerID);
-		$response=$bot->replyMessage($replyToken, $textMessageBuilder);
-			
-	}
+$response = $bot->getMessageContent($event['message']['id']);
+2.
+if ($response->isSucceeded()) {
+3.
+$dataBinary = $response->getRawBody();
+4.
+//chdir('admins');
+5.
+$fileFullSavePath = 'ชื่อรูปภาพ.jpg';
+6.
+file_put_contents($fileFullSavePath,$dataBinary);
+7.
 }
-echo"OK";
