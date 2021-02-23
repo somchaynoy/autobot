158aac7d957f801f8f96d349c2e504468f9d0340
somchay <55747347+somchaynoy@users.noreply.github.com>
2021-02-23 11:09:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 $response = $bot->getMessageContent($event['message']['id']);
-2.
 if ($response->isSucceeded()) {
-3.
 $dataBinary = $response->getRawBody();
-4.
 //chdir('admins');
-5.
 $fileFullSavePath = 'ชื่อรูปภาพ.jpg';
-6.
 file_put_contents($fileFullSavePath,$dataBinary);
-7.
+
 }
